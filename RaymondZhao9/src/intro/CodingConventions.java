03dc1dd504127d8e245d6064e5080d7cfef2b1f6
BT_1N3_10 <BT_1N3_10@BT_1N3_10.bktech.local>
2017-09-12 15:19:37 -0400

    lesson 9/12

---
+++ RaymondZhao9/src/intro/CodingConventions.java
@@ -0,0 +1,35 @@
+package intro;
+
+public class CodingConventions {
+
+	//FIELDS ARE AT THE TOP!!!
+	private String name;//NOTE: DECLARE FIELDS ONLY, INSTANTIATE IN THE CONSTRUCTOR
+	/*
+	 * in other words, don't do this:
+	 * private String name = "Mr. Nockles";
+	 */
+	private String description;
+	
+	/*
+	 * this is a constructor, it is like a method except the "return type" is an instance of the class 
+	 * for this reason, a constructor must ALWAYS be named after the class
+	 */
+	public CodingConventions(String name, int descriptionIndex){
+		//instantiate variables now:
+		
+		/*
+		 * there are two variables called "name" the local variable and the field
+		 * distinguish between the two using the reserved word "this" (field)
+		 */
+		this.name = name;
+		//this is a static call to a constant
+		description = IntroMain.DESCRIPTIONS[descriptionIndex];
+	}
+	
+	//normal method "void" is the return type
+	public void doStuff() {
+		//static method call:
+		String output = name + description; //user spaces in between operations 1 + 1 = 2, not 1+1=2
+		System.out.println(output);
+	}
+}
--- RaymondZhao9/src/intro/IntroMain.java
+++ RaymondZhao9/src/intro/IntroMain.java
@@ -1,6 +1,30 @@
-package intro;
+package intro; //package declaration: matches folder where this file is located 
 
+//class header
 public class IntroMain {
-//line of text 
-  //another line of text
+	
+	//this is a constant, they can be public
+	public static final String[] DESCRIPTIONS = {" is a teacher at BTHS", " is a student at BTHS"};
+	
+	//this is the first method that is executed. It is static because it is independent of instances
+	public static void main(String[] args) {
+		//this is the declaration of an instance of CodingConventions
+		//in the same line of code, it is also being instantiated 
+		//note the use of the word "new" this word is always used when calling constructors 
+		
+		int numberOfPeople = 10;
+		for(int i = 0; i < numberOfPeople; i++) {
+			CodingConventions conventionsInstance = new CodingConventions("Raymond", i % 2);
+			//a note on local variables: they are NOT FIELDS (HAS-A relationships)
+			//they are variables that are only used in the scope of a method
+			// after the method, they are destroyed
+			
+			/*
+			 * instance method call (the method belogns to the instance, not the class)
+			 */
+			conventionsInstance.doStuff();
+		}
+		
+		
+	}
 }
